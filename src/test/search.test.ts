79363ce22cdd8f64a709e6f26522009c6e9b1e0b
aspengs <jmf@grayshirts.com.ar>
2019-05-26 13:17:13 -0300

    --amend

--- src/test/search.test.ts
+++ src/test/search.test.ts
@@ -1,22 +1,21 @@
-import testGrups from '../utils/testGroup'
 import { isMutant } from "../utils/sentinel";
 
 let mutantSequence = ["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"];
-let mutantData = { name: "mutant", rows: mutantSequence, words: testGrups };
+let mutantData = { name: "mutant", rows: mutantSequence};
 
 let HumanSequence = ["TTGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CACCTA", "TCACTG"];
-let humantData = { name: "human", rows: HumanSequence, words: testGrups };
+let humantData = { name: "human", rows: HumanSequence};
 
 describe(`${mutantData.name} solver`, () => {
     it(`should find a ${mutantData.name} `, () => {
-        const matches = isMutant(mutantData.rows, mutantData.words, 2);
+        const matches = isMutant(mutantData.rows);
         expect(matches).toEqual(true)       
     })
 });
 
 describe(`${humantData.name} solver`, () => {
     it(`should find a ${humantData.name} `, () => {
-        const matches = isMutant(humantData.rows, humantData.words, 2);
+        const matches = isMutant(humantData.rows);
         expect(matches).toEqual(false)       
     })
 });
